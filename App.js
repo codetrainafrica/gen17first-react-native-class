d898571a3e2a4781a8c01c8c9dcbfd01092ef7fc
Mubarak Awal <awalmubarak6@gmail.com>
2021-08-07 14:40:22 +0000

    first react native class

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TextInput } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={[styles.box, styles.boxRight]}></View>
+			<View style={styles.box}></View>
+			<View style={styles.box}></View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 100,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'space-evenly',
+		// flexDirection: 'row',
+	},
+	box: {
+		flex: 30,
+		width: 50,
+		height: 50,
+		backgroundColor: 'green',
+		margin: 5,
+	},
+
+	boxRight: {
+		// alignSelf: 'center',
+		position: 'absolute',
+		top: 0,
+		left: 150,
+		right: 0,
+		bottom: 0,
+		backgroundColor: 'red',
+		zIndex: 100,
+	},
+
+	thanksText: {
+		fontSize: 20,
+		color: 'red',
+	},
+
+	logo: {
+		width: 300,
+		height: 300,
+	},
 });
